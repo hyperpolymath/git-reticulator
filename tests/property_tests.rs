59be81406a9c89c92fe2e93c9a714e70085de045
hyperpolymath <6759885+hyperpolymath@users.noreply.github.com>
2026-06-04 17:49:58 +0100

    chore: rename k9 directories to self-validating for clarity

--- tests/property_tests.rs
+++ tests/property_tests.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
-// Copyright (c) 2026 Jonathan D.A. Jewell (hyperpolymath) <j.d.a.jewell@open.ac.uk>
+// Copyright (c) Jonathan D.A. Jewell <j.d.a.jewell@open.ac.uk>
 //
 // tests/property_tests.rs
 // Property-based (P2P) tests for git-reticulator using proptest.
